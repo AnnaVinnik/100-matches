9f38e17ed4825429f8ae310ab0ef16c1e5b6a53a
Анастасия <>
2017-04-16 15:18:02 +0700

    Added library

--- main.c
+++ main.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
-#include ctest
-
+#include "ctest.h"
+#define max matches 100
+#include 100-matches.h
 
 int main
 {
-	
-	
+	int player = 1;
+	int ost;
+	int take;
+	ost = max matches
+	if player = 1
+	{
+		printf ("You move. In the table", ost, "matches");
+		printf ("How many matches You take?");
+		scanf ("%d", &take);
+	}
 	
 
 return 0;
