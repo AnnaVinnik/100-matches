2f3ccdbe7bc479dbfcf66f26f82b22a5ae5043d6
AnnaVinnik <vinnik.anna@rambler.ru>
2017-04-25 14:19:00 +0700

    Basis of game

--- main.c
+++ main.c
@@ -1,21 +1,36 @@
 #include <stdio.h>
-#include "ctest.h"
-#define max matches 100
-#include 100-matches.h
+#define max_matches 100
+#include "random.h"
 
-int main
+
+int main ()
 {
-	int player = 1;
-	int ost;
-	int take;
-	ost = max matches
-	if player = 1
-	{
-		printf ("You move. In the table", ost, "matches");
-		printf ("How many matches You take?");
-		scanf ("%d", &take);
-	}
-	
+    int player = 1, ost, take, y;
+    double k;
+    ost = max_matches;
 
-return 0;
+
+    printf ("You move. In the table %d matches \n", ost);
+    for (;ost > 0;){
+        printf ("How many matches You take? \n");
+        scanf("%d", &take);
+        ost = ost - take;
+        player = player + 1;
+        printf ("In the table %d matches \n", ost);
+
+    if (ost > 0){
+        take = getrand(1, 9);
+        ost = ost - take;
+        player = player + 1;
+        printf ("Компьютер сделал свой ход \n");
+        printf ("In the table %d matches \n\n", ost);
+    }
+}
+
+    if (player%2 == 0){
+        printf ("Вы проиграли\n");
+    } else {
+        printf( "Вы выиграли\n");
+    }
+    return 0;
 }
